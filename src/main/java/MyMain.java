7ad72f92cedc3945ea876adf93d70c3846947087
22sdhoat <22sdhoat@athenian.org>
2020-09-14 22:36:48 +0000

    added a function that calculates binary to decimal

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
-    }
-    
+        int input = Integer.parseInt(binary); 
+        int exponent = 0;
+        int decimal = 0;
+        while(input != 0) {
+            decimal += (input % 10) * Math.pow(2,exponent);
+            input /= 10;
+            exponent++;
+        }
+        return decimal;
+    }   
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+		System.out.print("Input Binary: ");
+		String binary_value = scan.nextLine();
+		System.out.println("Here is the value in decimal: " + binaryToDecimal(binary_value));
     }
 }
